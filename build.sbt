4933669236a583d737592a921b0d5295a51d53d0
Gunnar Bastkowski <gunnar@bastkowski.name>
2018-08-29 10:17:35 +0200

    Upgrade dependencies

--- build.sbt
+++ build.sbt
@@ -2,14 +2,12 @@ import Dependencies._
 
 name := "scalasciidoc"
 version := "1.0"
-scalaVersion := "2.11.7"
+scalaVersion := "2.12.6"
 
 libraryDependencies ++= Seq(
   parboiled,
   `scala-reflect`,
   `scala-parser-combinators`,
-  `logback-classic`,
-  `scala-logging`,
   scalacheck % Test,
   scalatest % Test
-)
+) ++ logging
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,16 +2,15 @@ import sbt._
 import Keys._
 
 object Dependencies {
+  val `log4j-scala`              = "org.apache.logging.log4j"        %% "log4j-api-scala"          % "11.0"
+  val `log4j-api`                = "org.apache.logging.log4j"        % "log4j-api"                 % "2.11.1"
+  val `log4j-core`               = "org.apache.logging.log4j"        % "log4j-core"                % "2.11.1"
+  val `scala-parser-combinators` = "org.scala-lang.modules"          %% "scala-parser-combinators" % "1.1.1"
+  val `scala-reflect`            = "org.scala-lang"                  %  "scala-reflect"            % "2.12.6"
+  val parboiled                  = "org.parboiled"                   %% "parboiled"                % "2.1.4"
+  val scalatest                  = "org.scalatest"                   %% "scalatest"                % "3.0.5"
+  val scalacheck                 = "org.scalacheck"                  %% "scalacheck"               % "1.14.0"
 
-  val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"
-  val `logback-classic` = "ch.qos.logback" % "logback-classic" % "1.1.3"
-  val `scala-parser-combinators` = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
-  val `scala-reflect` = "org.scala-lang" % "scala-reflect" % "2.11.7"
-  val parboiled = "org.parboiled" %% "parboiled" % "2.1.0"
-  val scalatest = "org.scalatest" %% "scalatest" % "2.2.1"
-  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.12.2"
-
-  val logging = Seq(`scala-logging`, `logback-classic`)
-  val testing = Seq(scalatest, scalacheck) map (_ % Test)
-
+  val logging                    = Seq(`log4j-scala`, `log4j-api`, `log4j-core`)
+  val testing                    = Seq(scalatest, scalacheck) map (_ % Test)
 }
